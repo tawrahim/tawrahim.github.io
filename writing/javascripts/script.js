4fbdc66b7243a45ffc361ac62239de201ad7382c
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2014-01-18 13:26:11 -0500

    clean up js function call

--- writing/javascripts/script.js
+++ writing/javascripts/script.js
@@ -1,15 +1,33 @@
-var Tawheed = {
+/**
+ * This is a collection of functions
+ * used in the project
+ * @author: Tawheed Abdul-Raheem
+ * @class: MainUtility 
+ * */
 
-    init:function () {
-        this.currentDate();
-    },
+var App = App || {};
 
-    currentDate: function () {
-        $("#mainDate").append(new Date());
-    },
+(function() {
+    'use strict';
 
-};
+    App.MainUtility = {
 
+        // Initialize all functions that needs
+        // to be invoked on page load
+        init: function() {
+            this.currentDate();
+        },
+
+        // Append current date to DOM
+        currentDate: function() {
+            $('#mainDate').append(new Date());
+        }
+    };
+    return App.MainUtility;
+}());
+
+// Invoke the init function
 $(function () {
-    Tawheed.init();
-})
+    'use strict';
+    App.MainUtility.init();
+});
